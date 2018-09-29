b3ffcb229c6f828a628d0026f6f5468b4af825ba
YiYeHuang <tonyhuang0446@gmail.com>
2018-09-28 21:31:23 -0700

    change project structure, using gradle to import dependency

---
+++ Lab1/src/main/java/com/royalchild/assignment/answer/GetDaysInMonth.java
@@ -0,0 +1,32 @@
+package com.royalchild.assignment.answer;
+
+public class GetDaysInMonth {
+
+	/**
+	 *
+	 * Days of Month
+	 *
+	 * Now that we know the way to print out the Days in a week
+	 * Let do days in a Month now.
+	 *
+	 * Given a input value 1 between 1 to 12 and given a input year
+	 * print out how many days exist in that given month
+	 *
+	 * hint: Feb in leap year has 29 days
+	 *
+	 *
+	 * expect: e.g. input 7 and 2010 should print 31
+	 * 				input 2 and 2000 should print 29
+	 *
+	 */
+	public static void main(String[] args) {
+
+		// the testing value should be between 1 to 12
+		int inputMonth = 1;
+		// the testing year value should be between valid year format
+		int inputYear = 2000;
+
+		// Write your program here
+
+	}
+}
