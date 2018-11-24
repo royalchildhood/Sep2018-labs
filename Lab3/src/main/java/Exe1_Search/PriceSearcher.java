ccaf76801178130935f634b3e061f6775e309261
Tony <tonyhuang0446@gmail.com>
2018-11-24 00:45:08 -0800

    questions refactor

--- Lab3/src/main/java/Exe1_Search/PriceSearcher.java
+++ Lab3/src/main/java/Exe1_Search/PriceSearcher.java
@@ -10,65 +10,55 @@ public class PriceSearcher {
 
 
     public static void main(String[] args) throws IOException {
+        /**=======================================================================
+         * Initial
+         * =======================================================================
+         */
         int numOfLines = 100000;
         int numOfPricePerLine = 1000;
         //PriceUtil.generatePriceList(numOfLines, numOfPricePerLine, 10000, 999900, "resource/pricelist.txt");
         double[] toSearch = PriceUtil.loadPriceList("resource/pricelist.txt", numOfLines*numOfPricePerLine);
 
+        /**=======================================================================
+         * search target
+         * =======================================================================
+         */
+        double searchTarget = 1234.01;
+        double lowerRange = 1111;
+        double upperRange = 9999;
 
+        /**=======================================================================
+         * Performance
+         * =======================================================================
+         */
         long startTime = System.currentTimeMillis();
-        normalSearch(8869.9D, toSearch);
+        normalSearch(searchTarget, toSearch);
         long endTime = System.currentTimeMillis();
         double second = endTime - startTime;
-        System.out.println("Normal Search took: " + second + " milliseconds" + "\n");
+        System.out.println("Normal Search took: " + second + " milliseconds to finish" + "\n");
 
         startTime = System.currentTimeMillis();
         findMax(toSearch);
         endTime = System.currentTimeMillis();
         second = endTime - startTime;
-        System.out.println("Max Search took: " + second + " milliseconds"+ "\n");
+        System.out.println("Max Search took: " + second + " milliseconds to finish"+ "\n");
 
         startTime = System.currentTimeMillis();
-        // findRange(223.11, 1119.23, toSearch);
+        findRange(lowerRange, upperRange, toSearch);
         endTime = System.currentTimeMillis();
         second = endTime - startTime;
-        System.out.println("Range Search took: " + second + " milliseconds"+ "\n");
-
-        startTime = System.currentTimeMillis();
-        SortUtil.sort(toSearch, 0, toSearch.length - 1);
-        endTime = System.currentTimeMillis();
-        second = endTime - startTime;
-        System.out.println("Sort took: " + second + " milliseconds"+ "\n");
-
-        startTime = System.currentTimeMillis();
-        BinarySearchUtil.findNumber(toSearch,8869.9D);
-        endTime = System.currentTimeMillis();
-        second = endTime - startTime;
-        System.out.println("binary search took: " + second + " milliseconds"+ "\n");
-
+        System.out.println("Range Search took: " + second + " milliseconds  to finish"+ "\n");
     }
 
     public static void normalSearch(double target, double[] list) {
-        for (int i = 0; i < list.length; i++) {
-            if (target == list[i]) {
-                System.out.println("find target: " + target);
-                return;
-            }
-        }
-        System.out.println("Did not find");
+        // TODO
     }
 
     public static void findMax(double[] list) {
-        double max = list[0];
-        for (int i = 0; i < list.length; i++) {
-            if (list[i] >= max) {
-                max = list[i];
-            }
-        }
-        System.out.println("The max item of the given list is: " + max);
+        // TODO
     }
 
-    public static void findRange(double min, double max,  double[] list) {
-
+    public static void findRange(double min, double max, double[] list) {
+        // TODO
     }
 }
---
+++ Lab3/src/main/java/Exe2_Matrix/PracticeMatrix.java
@@ -0,0 +1,24 @@
+package Exe2_Matrix;
+
+public class PracticeMatrix {
+
+    public static void printMatix() {
+        // TODO
+    }
+
+    public static void setMatix() {
+        // TODO
+    }
+
+    public static void flipMartix() {
+        // TODO
+    }
+
+    public static void flipMartixUpSizeDown() {
+        // TODO
+    }
+
+    public static void flipMartixSysmertic() {
+        // TODO
+    }
+}
