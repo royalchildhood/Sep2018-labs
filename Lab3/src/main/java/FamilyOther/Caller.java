d8bd301d0f1289902e291243395af8126cc68697
YiYeHuang <tonyhuang0446@gmail.com>
2019-01-26 21:37:34 -0800

    in class code example

---
+++ Lab3/src/main/java/FamilyOther/Caller.java
@@ -0,0 +1,10 @@
+package FamilyOther;
+
+import Family.Parent;
+
+public class Caller {
+	public static void main(String[] args) {
+		Parent p = new Parent("John");
+		//p.educationDirection();
+	}
+}
